a4b2f3cba4de9ea07a62bb98114ccbb374b5d3bf
sairohan <sairohan@students.cs.ubc.ca>
2023-11-21 19:00:17 -0800

    added buttons in home page (Phase_3 v3.1)

--- src/main/ui/GradeBookGUI.java
+++ src/main/ui/GradeBookGUI.java
@@ -12,8 +12,8 @@ import java.awt.event.ActionListener;
 public class GradeBookGUI extends JFrame {
     private JFrame hFrame;
     private JPanel leftLogo;
-    private JPanel mainActionBtns;
-    private JPanel lowerSmallBtns;
+    private JPanel imageLogoPanel;
+    private JPanel mainHomeBtnsPanel;
     private static final String JSON_LOCATION = "./data/CourseListGUI.json";
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
@@ -25,20 +25,15 @@ public class GradeBookGUI extends JFrame {
         jsonReader = new JsonReader(JSON_LOCATION);
     }
 
-    public void initializeLowerButtons() {
+    public void initializeActionButtons() {
         saveButton();
         quitButton();
         loadButton();
         addCourseButton();
+        removeCourseButton();
+        viewAllButton();
+        calculateGradeButton();
 
-    }
-
-
-    public void initializeMainButtons() {
-//        addCourseButton();
-//        removeCourseButton();
-//        viewAllButton();
-//        calculateButton();
     }
 
 
@@ -49,45 +44,47 @@ public class GradeBookGUI extends JFrame {
         hFrame.setVisible(true);
 //        frame.pack();
         hFrame.setSize(800, 650);
+        hFrame.setLayout(null);
         initializePanels();
-
 
 
     }
 
     public void initializePanels() {
-        leftLogo = new JPanel();
-        rightHomePanel();
-        lowerRightPanel();
+        logoPanel();
+        mainPanel();
 
     }
 
-//    private void leftHomePanel() {
-//
-//        //add images
-//    }
-//
-    private void rightHomePanel() {
-        mainActionBtns = new JPanel();
-        hFrame.getContentPane().add(mainActionBtns);
-        mainActionBtns.setLayout(null);
-        initializeMainButtons();
+    private void logoPanel() {
+        imageLogoPanel = new JPanel();
+        hFrame.add(imageLogoPanel);
+        imageLogoPanel.setLayout(null);
+        imageLogoPanel.setBackground(Color.green);
+        imageLogoPanel.setBounds(0, 0, 268, 622);
 
-        //add buttons add,remove, viewall, calculate
+        //add images, logos or gifs
     }
 
-    public void lowerRightPanel() {
-        lowerSmallBtns = new JPanel();
-        hFrame.add(lowerSmallBtns);
-        lowerSmallBtns.setLayout(null);
-//        lowerSmallBtns.setBounds(100, 50, 200, 100);
-        initializeLowerButtons();
+    public void mainPanel() {
+        mainHomeBtnsPanel = new JPanel();
+        hFrame.add(mainHomeBtnsPanel);
+        mainHomeBtnsPanel.setLayout(null);
+
+        mainHomeBtnsPanel.setBackground(Color.BLUE);
+        mainHomeBtnsPanel.setBounds(268, 0, 532, 622);
+        initializeActionButtons();
+
+
+
+
     }
 
     public void addCourseButton() {
         JButton addButn = new JButton("Add Course");
-        addButn.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-        addButn.setBounds(40, 60, 999, 40);
+        addButn.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
+        addButn.setBounds(185, 120, 190, 70);
+        addButn.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
         addButn.addActionListener(new ActionListener() {
 
@@ -96,15 +93,70 @@ public class GradeBookGUI extends JFrame {
                 System.exit(0);
             }
         });
-        lowerSmallBtns.add(addButn);
-
+        mainHomeBtnsPanel.add(addButn);
 
     }
 
+    public void removeCourseButton() {
+        JButton removeButn = new JButton("Remove Course");
+        removeButn.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
+        removeButn.setBounds(185, 220, 190, 70);
+        removeButn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        removeButn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        mainHomeBtnsPanel.add(removeButn);
+
+    }
+
+    public void viewAllButton() {
+        JButton viewAllButn = new JButton("View All Courses");
+        viewAllButn.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
+        viewAllButn.setBounds(185, 320, 190, 70);
+        viewAllButn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        viewAllButn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        mainHomeBtnsPanel.add(viewAllButn);
+
+    }
+
+    public void calculateGradeButton() {
+        JButton calcButn = new JButton("Progress Report");
+        calcButn.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
+        calcButn.setBounds(185, 420, 190, 70);
+        calcButn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        calcButn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        mainHomeBtnsPanel.add(calcButn);
+
+    }
+
+
+
+
     public void saveButton() {
         JButton saveButn = new JButton("Save");
-        saveButn.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-        saveButn.setBounds(480, 550, 80, 40);
+        saveButn.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+        saveButn.setBounds(230, 560, 90, 50);
+        saveButn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        //saveButn.setBounds(480, 550, 80, 40);
 
         saveButn.addActionListener(new ActionListener() {
 
@@ -113,15 +165,16 @@ public class GradeBookGUI extends JFrame {
                 System.exit(0);
             }
         });
-        lowerSmallBtns.add(saveButn);
+        mainHomeBtnsPanel.add(saveButn);
 
 
     }
 
     public void loadButton() {
         JButton loadButn = new JButton("Load");
-        loadButn.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-        loadButn.setBounds(280, 550, 80, 40);
+        loadButn.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+        loadButn.setBounds(30, 560, 90, 50);
+        loadButn.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
         loadButn.addActionListener(new ActionListener() {
 
@@ -130,17 +183,17 @@ public class GradeBookGUI extends JFrame {
                 System.exit(0);
             }
         });
-        lowerSmallBtns.add(loadButn);
+        mainHomeBtnsPanel.add(loadButn);
 
 
     }
 
 
-
     public void quitButton() {
         JButton quitButn = new JButton("Quit");
-        quitButn.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-        quitButn.setBounds(680, 550, 80, 40);
+        quitButn.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+        quitButn.setBounds(420, 560, 90, 50);
+        quitButn.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
         quitButn.addActionListener(new ActionListener() {
 
@@ -149,7 +202,7 @@ public class GradeBookGUI extends JFrame {
                 System.exit(0);
             }
         });
-        lowerSmallBtns.add(quitButn);
+        mainHomeBtnsPanel.add(quitButn);
 
     }
 //        quitButn.setBackground(new Color(255, 255, 255));
@@ -157,19 +210,6 @@ public class GradeBookGUI extends JFrame {
 
 
 //        frame.getContentPane().add(quitButn);
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
